3ec5f26183d7af14c246b8beed1987b6cf76d720
Dennis <McCoy>
2019-12-10 18:22:11 -0600

    terminado

--- App.js
+++ App.js
@@ -1,13 +1,20 @@
 import React, {Component} from 'react';
-import { StyleSheet, Text, View, TouchableOpacity, AsyncStorage, TextInput, FlatList, SafeAreaView } from 'react-native';
-
-
-//let DATA = [{tema:'hola', url:'adios'}]
+import { StyleSheet, Text, View, TouchableOpacity, AsyncStorage, TextInput, FlatList, SafeAreaView, Linking, Button, ScrollView} from 'react-native';
+//import { hide } from 'expo/build/launch/SplashScreen';
 
 function Item({ title }) {
+  
+  _handleOpenWithLinking = () => {
+    let vari=JSON.stringify({title})
+    var obj=JSON.parse(vari);
+    Linking.openURL(obj.title);
+
+  }
   return (
     <View style={styles.item}>
-      <Text style={styles.title}>{title}</Text>
+      <TouchableOpacity onPress={this._handleOpenWithLinking}>
+      <Text>{title}</Text>
+      </TouchableOpacity>
     </View>
   );
 }
@@ -20,46 +27,49 @@ export default class App extends Component {
     this.state = {
       tema: '',
       url:'',
-      url1:'',
-      DATA:['a','a'],
+      DATA:[],
     };
   }
-  
   //guardar
-  saveItem = async (key, value) => {
+  //saveItem = async (key, value) => {
+    saveItem = async (key, value) => {
     try {
-      await AsyncStorage.setItem(key, JSON.stringify(value));
+      await AsyncStorage.setItem(key, value);
+      //await AsyncStorage.setItem(key, JSON.stringify(value));
       //this.setState({tema:''});
       //this.setState({url:''});
       //this.setState({url1:''});
     } catch (error) {
-      // Error saving data
       console.log("Saving data error");
     }
   };
   saveStorage =() =>{
-    this.saveItem(this.state.tema,{"url1":this.state.url, "url2":this.state.url1})
+    //this.saveItem(this.state.tema,{"url1":this.state.url, "url2":this.state.url1})
+    this.saveItem(this.state.tema,this.state.url)
+    this.getData();
   }
   //obtener
   showItem = async (key) => {
     try {
       const value = await AsyncStorage.getItem(key);
       if (value !== null) {
-        // We have data!!
         return value;
       }else{
         console.log("Read data error");
       }
     } catch (error) {
-      // Error retrieving data
       console.log("Read data error");
     }
   };
   showStorage =() =>{
-    this.showItem(this.state.tema).then(result=>{
+    /*this.showItem(this.state.tema).then(result=>{
       let Obj=JSON.parse(result)
-      alert("??"+Obj.Aa+"=>"+Obj.url1+" :: "+Obj.url2);
-    })
+      alert(Obj);
+    })*/
+    AsyncStorage.getItem(this.state.tema, (err, result) => {
+      let ur=result
+      alert(ur.tema);
+    });
   }
   //remove
   removeItem = async (key) => {
@@ -72,6 +82,7 @@ export default class App extends Component {
   };
   removeStorage =() =>{
     this.removeItem(this.state.tema);
+    this.getData();
   }
   //temas
   getData = async () => {
@@ -80,11 +91,16 @@ export default class App extends Component {
     AsyncStorage.getAllKeys((err, keys) => {
       AsyncStorage.multiGet(keys, (err, stores) => {
         stores.map((result, i, store) => {
-          // get at each store's key/value so you can work with it
-           key = store[i][0];
-           todo=todo+key+" ";
-           key=todo.split(" ")
+           key = ">>>"+store[i][0]+"<<< "+store[i][1];
+           if(todo!=""){
+            todo=todo+" "+key;
+            key=todo.split(" ")
+           }else{
+            todo=todo+key;
+           }
+           
           //let value = store[i][1];
+          //alert(value)
           
         });
         
@@ -96,48 +112,58 @@ export default class App extends Component {
     });
   };
 
+  //borrar todo
+  clearAsyncStorage = async() => {
+    AsyncStorage.clear();
+  }
+  //abrir link
+  _handleOpenWithLinking = () => {
+    Linking.openURL('https://expo.io');
+  }
+
   render(){
     
     let {container, btnStyle, txtStyle}=styles;
     return (
-      
       <View style={styles.container}>
-         
-        <TextInput
-        placeholder="Tema"
-        value={this.state.tema}
-        onChangeText={data=>this.setState({tema:data})}
-        />
-        <TextInput
-        placeholder="URL1"
-        value={this.state.url}
-        onChangeText={data=>this.setState({url:data})}
-        />
-        <TextInput
-        placeholder="URL"
-        value={this.state.url1}
-        onChangeText={data=>this.setState({url1:data})}
-        />
-        <TouchableOpacity style={styles.btnStyle} onPress={this.saveStorage}> 
-          <Text style={txtStyle}>Save Data</Text>
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.btnStyle} onPress={this.showStorage}>
-          <Text style={txtStyle}>Read Data</Text>
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.btnStyle} onPress={this.removeStorage}>
-          <Text style={txtStyle}>Remove Data</Text>
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.btnStyle} onPress={this.getData}>
-          <Text style={txtStyle}>todo</Text>
-        </TouchableOpacity>
-        
-        <SafeAreaView style={styles.container}>
+        <Text style={styles.Titulo}>Biblioteca de URL's</Text>
+        <View style={styles.formulario}>
+        <View style={styles.formulario2}>
+          <TextInput style={styles.input}
+          placeholder="Tema"
+          value={this.state.tema}
+          onChangeText={data=>this.setState({tema:data})}
+          />
+          <TextInput style={styles.input}
+          placeholder="URL"
+          value={this.state.url}
+          onChangeText={data=>this.setState({url:data})}
+          />
+          </View>
+          <TouchableOpacity style={styles.btnStyle} onPress={this.saveStorage}> 
+            <Text style={txtStyle}>Save Data</Text>
+          </TouchableOpacity>
+        </View>
+        <View style={styles.lista}>
+        <SafeAreaView style={styles.safe}>
           <FlatList
             data={this.state.DATA}
             renderItem={({ item }) => <Item title={item} />}
             //keyExtractor={item => item.tema}
           />
         </SafeAreaView>
+        </View>
+        <View style={styles.ultimos}> 
+        <TouchableOpacity style={styles.btnbajos} onPress={this.removeStorage}>
+          <Text style={txtStyle}>Remove Data</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.btnbajos} onPress={this.getData}>
+          <Text style={txtStyle}> ver lista</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.btnbajos} onPress={this.clearAsyncStorage}>
+          <Text style={txtStyle}>borrar todo</Text>
+        </TouchableOpacity>
+        </View>
       </View>
     );
   }
@@ -146,40 +172,77 @@ export default class App extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#F6F4DA',
     alignItems: 'center',
     justifyContent: 'center',
     padding:'5%'
   },
-  btnStyle:{
-    backgroundColor: '#7f7fff',
-    justifyContent:'center',
-    height: 50,
-    width:100,
-    marginTop:10,
-    alignItems:'center'
+  Titulo: {
+    fontSize:50,
+    color:'#7A7757',
+    fontWeight: 'bold'
   },
-  item: {
-    backgroundColor: '#f9c2ff',
-    padding: 20,
-    marginVertical: 8,
-    marginHorizontal: 16,
+  formulario:{
+    flex:2,
+    flexDirection:'row',
   },
-  title: {
-    fontSize: 32,
+  formulario2:{
+    flex:3,
+    flexDirection:'column',
   },
-});/*
+  input:{
+    backgroundColor:'#fff',
+    margin: 8,
+    height: 42,
+    borderRadius:10,
 
-        {
-          id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
-          title: 'First Item',
-        },
-        {
-          id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f63',
-          title: 'Second Item',
-        },
-        {
-          id: '58694a0f-3da1-471f-bd96-145571e29d72',
-          title: 'Third Item',
-        },
-      */ 
+  },
+  btnStyle:{
+    width:100,
+    height:100,
+    alignItems:'center',
+    alignContent:'center',
+    backgroundColor:'#B3EA36',
+    margin:8,
+    justifyContent:'center',
+    flex:1,
+    borderRadius:20,
+  },
+  lista:{
+    flex:4,
+    alignItems:'center',
+  },
+  safe:{
+    width:'100%',
+    height:'100%',
+    backgroundColor:'#DEF0B5',
+    padding:8,
+    alignItems:'center',
+    alignContent:'center',
+    justifyContent:"center",
+  },
+  ultimos:{
+    flex:1,
+    flexDirection:'row',
+  },
+  btnbajos:{
+    width:100,
+    height:50,
+    alignItems:'center',
+    alignContent:'center',
+    backgroundColor:'#B3EA36',
+    margin:8,
+    justifyContent:"center",
+    flex:1,
+    borderRadius:20,
+  },
+  txtStyle:{
+    color:'#121900',
+    fontSize:15,
+  },
+  item:{
+    alignSelf:"center",
+    margin:6,
+    fontSize: 50,
+  }
+});
